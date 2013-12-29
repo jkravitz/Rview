3f6dae0f74620e577dedd2a70b70e04c1b8335d0
Jordan Kravitz <jkravitz61@gmail.com>
2013-12-29 15:02:50 -0600

    meh

--- rviewlogic.js
+++ rviewlogic.js
@@ -48,54 +48,51 @@ document.addEventListener('DOMContentLoaded', function () {
             container = document.getElementById('imageLinkToClickOn').appendChild(image);
 
 
+          });      
+      }
+      else if(imagesToZoomList[i].href.indexOf('imgur.com')!=-1){ 
+        //need to verify link before anonymous functions
+        imagesToZoom[i].className = tempClassName + " photoPopUp";
+
+        imagesToZoom[i].id=i;
+        var link= document.getElementById(i);
+        //click logic for clicking on thumbnail
+        var tempNum = i;
+        link.addEventListener('click', function(link){
+        //function that occurs when a thumbnail is clicked
+          link.preventDefault();
+          var divContainer = document.createElement('div');
+          divContainer.setAttribute('id', 'photoThing');
+          document.body.appendChild(divContainer);
+          divContainer.addEventListener('click', function(){
+             document.getElementById('photoThing').remove();
           });
-     // }
-      
-  }
-  else if(imagesToZoomList[i].href.indexOf('imgur.com')!=-1){ 
-    //need to verify link before anonymous functions
-    imagesToZoom[i].className = tempClassName + " photoPopUp";
+          divContainer.addEventListener('backbutton', function(){
+            document.getElementById('photoThing').remove();
+          });  
+          var startIndex= this.href.indexOf('imgur.com/')+10;//relevant part of link
+          var endIndex=startIndex;
+          while(this.href.charAt(endIndex))
+            endIndex++;//find the end index
+          var substring = this.href.substring(startIndex, endIndex);
 
-    imagesToZoom[i].id=i;
-    var link= document.getElementById(i);
-    //click logic for clicking on thumbnail
-    var tempNum = i;
-    link.addEventListener('click', function(link){
-    //function that occurs when a thumbnail is clicked
-      link.preventDefault();
-      var divContainer = document.createElement('div');
-      divContainer.setAttribute('id', 'photoThing');
-      document.body.appendChild(divContainer);
-      divContainer.addEventListener('click', function(){
-         document.getElementById('photoThing').remove();
-      });
-      divContainer.addEventListener('backbutton', function(){
-        document.getElementById('photoThing').remove();
-      });  
-      var startIndex= this.href.indexOf('imgur.com/')+10;//relevant part of link
-      var endIndex=startIndex;
-      while(this.href.charAt(endIndex))
-        endIndex++;//find the end index
-      var substring = this.href.substring(startIndex, endIndex);
+          //create link to click on image to view original link
+          var alink = document.createElement("a");
+          alink.setAttribute("href", this.href);
+          alink.setAttribute("id", "imageLinkToClickOn" )
 
-      //create link to click on image to view original link
-      var alink = document.createElement("a");
-      alink.setAttribute("href", this.href);
-      alink.setAttribute("id", "imageLinkToClickOn" )
+          var image = document.createElement("img");
+          image.setAttribute("src", 'http://i.imgur.com/'+ substring + '.gif');
+          if(image.width>0 &&image.height>0){//real image, not an album
+            image.setAttribute("id", "actualPhoto");
+            image.setAttribute("alt", this.href);
+            var container= document.getElementById('photoThing').appendChild(alink);
+            container = document.getElementById('imageLinkToClickOn').appendChild(image);
+          }
 
-      var image = document.createElement("img");
-      image.setAttribute("id", "actualPhoto");
-      //need to do check here to see if this is an album or if sigle photo
-      //single photo-> we good
-      //album -> abort, just go to imgur
-      image.setAttribute("src", 'http://i.imgur.com/'+ substring + '.gif');
-      image.setAttribute("alt", "");
+          });
 
-      var container= document.getElementById('photoThing').appendChild(alink);
-      container = document.getElementById('imageLinkToClickOn').appendChild(image);
-      });
-
-  }
+      }
   i++;
   }
 
